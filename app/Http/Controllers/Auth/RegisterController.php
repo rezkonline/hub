7b2b7a5c6eaa2c752fd23fafb76b630caca80e35
AbdallahMohammed <abdallah.r660@gmail.com>
2022-02-12 03:45:03 +0200

    fix: register controller

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -54,8 +54,9 @@ class RegisterController extends Controller
         return Validator::make($data, [
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+            'phone' => ['required', 'numeric', 'unique:users'],
             'password' => ['required', 'string', 'min:8', 'confirmed'],
-        ]);
+        ], [], trans('customers.attributes'));
     }
 
     /**
@@ -69,6 +70,7 @@ class RegisterController extends Controller
         return User::create([
             'name' => $data['name'],
             'email' => $data['email'],
+            'phone' => $data['phone'],
             'type' => User::CUSTOMER_TYPE,
             'password' => Hash::make($data['password']),
         ]);
